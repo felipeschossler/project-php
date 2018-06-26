728b7f26e28c3d27f1e07cc613d3a48fa76a12b8
Felipe Schossler <felipealvesschossler@gmail.com>
2018-06-25 22:44:18 -0300

    i changed the way of database was created and i added table tiposDeMovimento

--- script_crud.sql
+++ script_crud.sql
@@ -1,6 +1,6 @@
 --cria o banco de dados
 
-CREATE DATABASE banco;
+CREATE DATABASE banco CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 --tabela agencia
 
@@ -14,16 +14,28 @@ CREATE TABLE cliente
 ( idCliente INT NOT NULL AUTO_INCREMENT , nome VARCHAR(40) NOT NULL , cidade VARCHAR(40) NOT NULL , cpf VARCHAR(11) NOT NULL , tipoCliente CHAR(1) NOT NULL , PRIMARY KEY (idCliente))
 ENGINE = InnoDB;
 
---tabela tiposdeconta
+--tabela tiposDeConta
 
-CREATE TABLE tiposConta 
-( idTipo INT NOT NULL AUTO_INCREMENT , descricao VARCHAR(30) NOT NULL , classe CHAR NOT NULL , PRIMARY KEY (idTipo)) 
+CREATE TABLE tiposDeConta 
+( idTipoConta INT NOT NULL AUTO_INCREMENT , descricao VARCHAR(30) NOT NULL , classe CHAR NOT NULL , PRIMARY KEY (idTipoConta)) 
 ENGINE = InnoDB;
 
---inserts na tabela
+--inserts na tabela tiposDeConta
 
-INSERT INTO tiposConta (descricao,classe) VALUES ('Conta Corrente Especial','C');
-INSERT INTO tiposConta (descricao,classe) VALUES ('Conta Corrente','C');
-INSERT INTO tiposConta (descricao,classe) VALUES ('Poupança','P');
-INSERT INTO tiposConta (descricao,classe) VALUES ('CDB','A');
+INSERT INTO tiposDeConta (descricao,classe) VALUES ('Conta Corrente Especial','C');
+INSERT INTO tiposDeConta (descricao,classe) VALUES ('Conta Corrente','C');
+INSERT INTO tiposDeConta (descricao,classe) VALUES ('Poupança','P');
+INSERT INTO tiposDeConta (descricao,classe) VALUES ('CDB','A');
 
+--tabela tiposDeMovimento
+
+CREATE TABLE tiposDeMovimento 
+( idTipoMov INT NOT NULL AUTO_INCREMENT , descricao VARCHAR(30) NOT NULL , tipoPagamento VARCHAR(1) NOT NULL , PRIMARY KEY (idTipoMov))
+ENGINE = InnoDB;
+
+--inserts na tabela tiposDeMovimento
+
+INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Retirada em Dinheiro', 'Débito');
+INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Depósito em Dinheiro', 'Crédito');
+INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Tarifa', 'Débito');
+INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Crédito de Juros ', 'Crédito');
