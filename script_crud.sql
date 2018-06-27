79e357888b7350be6eb4037ac1a0b35db7addfbf
Felipe Alves Schossler <felipes@matriz.sicred.com>
2018-06-27 17:51:58 -0300

    Adicionei a table conta e indentei corretamente o script

--- script_crud.sql
+++ script_crud.sql
@@ -5,19 +5,36 @@ CREATE DATABASE banco CHARACTER SET utf8 COLLATE utf8_general_ci;
 /*tabela agencia*/
 
 CREATE TABLE agencia 
-( idAgencia INT NOT NULL AUTO_INCREMENT , nome VARCHAR(40) NOT NULL , cidade VARCHAR(40) NOT NULL , PRIMARY KEY (idAgencia)) 
+( 
+    idAgencia INT NOT NULL AUTO_INCREMENT , 
+    nome VARCHAR(40) NOT NULL , 
+    cidade VARCHAR(40) NOT NULL , 
+    PRIMARY KEY (idAgencia)
+) 
 ENGINE = InnoDB;
 
 /*tabela cliente*/
 
 CREATE TABLE cliente 
-( idCliente INT NOT NULL AUTO_INCREMENT , nome VARCHAR(40) NOT NULL , cidade VARCHAR(40) NOT NULL , cpf VARCHAR(11) NOT NULL , tipoCliente CHAR(1) NOT NULL , PRIMARY KEY (idCliente))
+( 
+    idCliente INT NOT NULL AUTO_INCREMENT , 
+    nome VARCHAR(40) NOT NULL , 
+    cidade VARCHAR(40) NOT NULL , 
+    cpf VARCHAR(11) NOT NULL , 
+    tipoCliente CHAR(1) NOT NULL , 
+    PRIMARY KEY (idCliente)
+)
 ENGINE = InnoDB;
 
 /*tabela tiposDeConta*/
 
 CREATE TABLE tiposDeConta 
-( idTipoConta INT NOT NULL AUTO_INCREMENT , descricao VARCHAR(30) NOT NULL , classe CHAR NOT NULL , PRIMARY KEY (idTipoConta)) 
+( 
+    idTipoConta INT NOT NULL AUTO_INCREMENT , 
+    descricao VARCHAR(30) NOT NULL , 
+    classe CHAR NOT NULL , 
+    PRIMARY KEY (idTipoConta)
+) 
 ENGINE = InnoDB;
 
 /*inserts na tabela tiposDeConta*/
@@ -30,7 +47,12 @@ INSERT INTO tiposDeConta (descricao,classe) VALUES ('CDB','A');
 /*tabela tiposDeMovimento*/
 
 CREATE TABLE tiposDeMovimento 
-( idTipoMov INT NOT NULL AUTO_INCREMENT , descricao VARCHAR(30) NOT NULL , tipoPagamento VARCHAR(1) NOT NULL , PRIMARY KEY (idTipoMov))
+( 
+    idTipoMov INT NOT NULL AUTO_INCREMENT , 
+    descricao VARCHAR(30) NOT NULL , 
+    tipoPagamento VARCHAR(1) NOT NULL , 
+    PRIMARY KEY (idTipoMov)
+)
 ENGINE = InnoDB;
 
 /*inserts na tabela tiposDeMovimento*/
@@ -39,3 +61,20 @@ INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Retirada em Din
 INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Depósito em Dinheiro', 'Crédito');
 INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Tarifa', 'Débito');
 INSERT INTO tiposdemovimento (descricao, tipoPagamento) VALUES ('Crédito de Juros ', 'Crédito');
+
+/*tabela conta*/
+
+CREATE TABLE conta
+( 
+    idConta INT NOT NULL AUTO_INCREMENT , 
+    idAgencia INTEGER NOT NULL , 
+    idCliente INTEGER NOT NULL , 
+    limite FLOAT(10) NOT NULL , 
+    dataAbertura DATE NOT NULL , 
+    idTipoConta INTEGER NOT NULL , 
+    PRIMARY KEY (idConta) ,
+    CONSTRAINT fk_AgeConta FOREIGN KEY (idAgencia) REFERENCES agencia (idAgencia) , 
+    CONSTRAINT fk_CliCon FOREIGN KEY (idCliente) REFERENCES cliente (idCliente) , 
+    CONSTRAINT fk_TipoContaConta FOREIGN KEY (idTipoConta) REFERENCES tiposDeConta (idTipoConta)   
+)
+ENGINE = InnoDB;
